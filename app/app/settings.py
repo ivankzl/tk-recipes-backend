fc1c0863a4feccee5d6ddfc7b69b7e372a70f1c5
Ivan Kuzel <ivan.kuzel@travelperk.com>
2021-07-02 10:41:12 -0300

    Added environ dependency to allow env vars

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,15 +12,20 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cm0bh2!nn6b$jygu25bcckhcu1m%-$w)yt)rzsj&djd@d6u7&q'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
